70e2b0eeff957a885a4068393d7df5eef12579a6
harshit shukla <73156294+harshit-design@users.noreply.github.com>
2023-03-18 11:16:15 +0530

    Update Routes.js

--- src/Pages/Routes.js
+++ src/Pages/Routes.js
@@ -1,13 +1,17 @@
-import {Link, Routes, Route, useNavigate} from 'react-router-dom';
+import {Link, Routes, Route, useNavigate, Form, BrowserRouter} from 'react-router-dom';
 import React from 'react';
 import Database from './Database';
+import User_form_ from './UserForm';
 
 export default function Page_Route() {
     return(
         <div>
-            <Routes>
-                <Route path="/Database" element={<Database/>}/>
-            </Routes>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="user_form_" element={<User_form_/>}/>
+                    <Route path="/Database" element={<Database/>}/>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
